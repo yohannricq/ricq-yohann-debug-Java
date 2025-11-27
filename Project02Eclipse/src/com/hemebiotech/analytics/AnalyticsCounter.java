264f264288efbe9714a5e797c0c9a60bba5ce8c6
yohannricq <yohann.ricq@gmail.com>
2025-11-27 21:51:35 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,12 +3,51 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
-	
+
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
+
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public List<String> getSymptoms() {
+
+        return reader.GetSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String symptom : symptoms) {
+            map.put(symptom, map.getOrDefault(symptom, 0) + 1);
+        }
+
+        return map;
+    }
+
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+
+        return new TreeMap<>(symptoms);
+    }
+
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+        writer.writeSymptoms(symptoms);
+    }
+
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
